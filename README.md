a7fe678a7496bced41552bb6d5bec224cb7328eb
Michael Bass <littlebass09>
2026-02-02 09:03:34 -0800

    MB Initial push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ArcticFoxDailyChallenge_2_2_2026
+
+Complete the steps listed in dog_analysis.py
---
+++ dog_analysis.py
@@ -0,0 +1,20 @@
+
+
+
+### Kits that willl be helpful - Data, View, CreateRanges, DataframePivot
+### The focus of this challenge is creating categories, pivoting, and getting counts for category matches
+
+## We will count the number of Dalmations in the dataset from ages 4 to 7! How cute!
+
+
+# Load the dog_speeds.csv
+
+# Create labels / ranges to split the dogs into age categories
+# Note, we primarily want to know the dogs aged 4 to 7
+
+# Pivot the table to see the count of each breed for each age range
+
+# Get the number of Dalmations aged 4 to 7 and submit the count!
+
+
+
